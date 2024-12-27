9aced66f1a0738912a6cb03d0e9b476f546914d1
Vikrant Yadav <vikrantmani45@gmail.com>
2024-12-27 13:26:55 +0530

    generate-image api done

--- backend/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/GenAIController.java
+++ backend/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/GenAIController.java
@@ -1,16 +1,23 @@
 package com.ai.SpringAiDemo;
 
+import java.io.IOException;
+
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 
 public class GenAIController {
   ChatService chatService;
+  ImageService imageService;
 
   public GenAIController(ChatService chatService) {
     this.chatService = chatService;
+    this.imageService = imageService;
   }
 
   @GetMapping("/ask-ai")
@@ -24,5 +31,12 @@ public class GenAIController {
   @GetMapping("/ask-ai-options")
   public String getResponseOptions(@RequestParam String prompt) {
     return chatService.getResponseOptions(prompt);
+  }
+
+  @GetMapping("/generate-image")
+  public void generateImages(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+    ImageResponse imageResponse = imageService.generateImage(prompt);
+    String imageUrl = imageResponse.getResult().getOutput().getUrl();
+    response.sendRedirect(imageUrl);
   }
 }
