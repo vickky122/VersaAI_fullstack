6ec9dbd4ef4ae0dbfeea636c6dad7d5450af5a7c
Vikrant Yadav <vikrantmani45@gmail.com>
2024-12-27 13:03:39 +0530

    Building Service to generate image

--- backend/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/ImageService.java
+++ backend/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/ImageService.java
@@ -1,8 +1,23 @@
 package com.ai.SpringAiDemo;
 
+import org.springframework.ai.image.Image;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImageService {
+  private final OpenAiImageModel openAiImageModel;
+
+  public ImageService(OpenAiImageModel openAiImageModel) {
+    this.openAiImageModel = openAiImageModel;
+  }
+
+  public ImageResponse generateImage(String prompt) {
+    ImageResponse imageResponse = openAiImageModel.call(
+        new ImagePrompt(prompt));
+    return imageResponse;
+  }
 
 }
