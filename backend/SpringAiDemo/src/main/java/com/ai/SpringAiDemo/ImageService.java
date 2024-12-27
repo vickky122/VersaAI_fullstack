5de0d237971c860b097104720a0d786a9af9a4ba
Vikrant Yadav <vikrantmani45@gmail.com>
2024-12-27 14:32:03 +0530

    making the number of images generation more than 1

--- backend/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/ImageService.java
+++ backend/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/ImageService.java
@@ -4,6 +4,7 @@ import org.springframework.ai.image.Image;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,8 +16,16 @@ public class ImageService {
   }
 
   public ImageResponse generateImage(String prompt) {
+    // ImageResponse imageResponse = openAiImageModel.call(
+    // new ImagePrompt(prompt));
+
     ImageResponse imageResponse = openAiImageModel.call(
-        new ImagePrompt(prompt));
+        new ImagePrompt(prompt,
+            OpenAiImageOptions.builder()
+                .withQuality("hd")
+                .withN(2)
+                .withHeight(1024)
+                .withWidth(1024).build()));
     return imageResponse;
   }
 
