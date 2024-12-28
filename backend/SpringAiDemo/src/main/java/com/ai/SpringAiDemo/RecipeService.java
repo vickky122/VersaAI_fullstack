7e3b168780889a7adb43c3e3c15f41e273b5f16d
Vikrant Yadav <vikrantmani45@gmail.com>
2024-12-28 12:56:18 +0530

    getting started with the recipe

--- backend/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/RecipeService.java
+++ backend/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/RecipeService.java
@@ -1,8 +1,14 @@
 package com.ai.SpringAiDemo;
 
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RecipeService {
 
+  private final ChatModel chatModel;
+
+  public RecipeService(ChatModel chatModel) {
+    this.chatModel = chatModel;
+  }
 }
