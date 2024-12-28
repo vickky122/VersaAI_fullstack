c6493283e72966b52e0c85b6858cf882ae7989f2
Vikrant Yadav <vikrantmani45@gmail.com>
2024-12-28 14:46:55 +0530

    conditional rendering

--- frontend/spring-ai-react/src/App.js
+++ frontend/spring-ai-react/src/App.js
@@ -5,15 +5,27 @@ function App() {
   const [activeTab, setActiveTab] = useState('image-generator');
 
   const handleTabChange = (tab)=>{
-    alert(tab)
+    //alert(tab)
     setActiveTab(tab);
   }
 
 return(
   <div className="App">
-    <button onClick={()=>handleTabChange('image-generator')}>Image Generator</button>
-    <button onClick={()=>handleTabChange('chat')}>Chat</button>
-    <button onClick={()=>handleTabChange('recipe-generator')}>Recipe Generator</button>
+    <button onClick={()=>handleTabChange('image-generator')}>
+      Image Generator
+      </button>
+    <button onClick={()=>handleTabChange('chat')}>
+      Chat
+      </button>
+    <button onClick={()=>handleTabChange('recipe-generator')}>
+      Recipe Generator
+      </button>
+
+      <div>
+        {activeTab === 'image-generator' && <h1>Image Generator</h1>}
+        {activeTab === 'chat' && <h1>Chat</h1>} 
+        {activeTab === 'recipe-generator' && <h1>Recipe Generator</h1>}
+      </div>
     </div>
     );
   }
