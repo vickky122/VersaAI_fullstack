18295d3584e183e6257146fcc6dcda0376465b54
Vikrant Yadav <vikrantmani45@gmail.com>
2024-12-28 13:05:09 +0530

    initial recipe need

--- backend/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/RecipeService.java
+++ backend/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/RecipeService.java
@@ -11,4 +11,17 @@ public class RecipeService {
   public RecipeService(ChatModel chatModel) {
     this.chatModel = chatModel;
   }
+
+  public String createRecipe(String ingredients,
+      String cuisine,
+      String dietaryRestrictions) {
+    var template = """
+        I want to create a recipe using the following ingredients: {ingredients}.
+        The cuisine type I prefer is {cuisine}.
+        Please consider the following dietary restrictions: {dietaryRestrictions}.
+        Please provide me with a detailed recipe including title, list of ingredients, and cooking instructions
+        """;
+
+  }
+
 }
