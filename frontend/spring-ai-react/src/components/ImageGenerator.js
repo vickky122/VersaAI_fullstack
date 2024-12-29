e4ec423a2afb3b11125a7751b704c090152bc68c
Vikrant Yadav <vikrantmani45@gmail.com>
2024-12-29 10:26:21 +0530

    defining the input for image generator

--- frontend/spring-ai-react/src/components/ImageGenerator.js
+++ frontend/spring-ai-react/src/components/ImageGenerator.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ImageGenerator() {
+  const [prompt, setPrompt] = useState('');
+  const [imageUrls, setImageUrls] = useState([]);
   return (
-    <h2>ImageGenerator</h2>
+    <div className='tab-content'>
+<h2>ImageGenerator</h2>
+     <input
+        type="text"
+        placeholder="Enter a prompt"
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+
+    </div>
+    
   );
 }
 
