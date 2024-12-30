d2c1a014a58d16752941cff64ddcb2549a3abead
Vikrant Yadav <vikrantmani45@gmail.com>
2024-12-30 13:29:58 +0530

    integrating theme

--- frontend/spring-ai-react/src/index.js
+++ frontend/spring-ai-react/src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import darkTheme from './theme';
+import { ThemeContextProvider } from './ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
+    <ThemeContextProvider>
       <App />
-    </ThemeProvider>
+    </ThemeContextProvider>
   </React.StrictMode>
 );
 
